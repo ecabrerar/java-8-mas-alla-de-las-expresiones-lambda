4216fbe132ab6e33df8474d216c825d1407aa7f5
ecabrerar <eudris@gmail.com>
2017-02-22 14:29:17 -0400

    using method peek()

--- barcamprd2015/ejemplos/java8-demo/src/main/java/org/ecabrerar/examples/java8/demo/StreamBasicos.java
+++ barcamprd2015/ejemplos/java8-demo/src/main/java/org/ecabrerar/examples/java8/demo/StreamBasicos.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -17,75 +16,70 @@ import java.util.stream.Collectors;
  * @author ecabrerar
  */
 public class StreamBasicos {
-    
-    
+
+
     private void ejecutar(){
         System.out.println("Stream Básicos");
-        
+
         System.out.println("Ejecutando ejercicio 1");
         ejercicio1();
-        
+
         System.out.println("Ejecutando ejercicio 2");
-        ejercicio2();       
-       
+        ejercicio2();
+
     }
-    
+
     /**
      * Procesar la siguiente lista para que solamente contenga cadena de caracteres impares
      */
     private void ejercicio1() {
-        
+
         List<String> list = Arrays.asList(
         "BarCamp", "MongoDB", "10Gen", "TokuMX", "Nagios", "PUCMM", "Ruby", "JSON", "JSON");
-    
-    
-//      List<String> newList = list
-//            .stream()
-//            .filter(new Predicate<String>() {
-//
-//            @Override
-//            public boolean test(String t) {
-//               return (t.length() % 2)==1;
-//            }
-//        }).map(String::toLowerCase)
-//           .peek(System.out::println) 
-//          .collect(Collectors.toList());
-          
-       
+
+
+      List<String> newList = list
+            .stream()
+            .filter(s -> (s.length() % 2)==1)
+            .map(String::toLowerCase)
+           .peek(System.out::println)
+          .collect(Collectors.toList());
+
+
      List<String> newList1 = list
             .stream()
             .filter(s -> (s.length() % 2)==1)
             .map(String::toLowerCase)
-            .peek(s -> s.toString()) 
+            .peek(s -> s.toString())
             .collect(Collectors.toList());
-                   
-    
-        //newList.forEach(System.out::println);
+
+
+       // newList1.forEach(System.out::println);
     }
 
        /**
       * Convertir par de datos del map en un stream
-      * 
-     */    
+      *
+     */
     private void ejercicio2(){
         Map<String, Integer> map = new TreeMap<>();
         map.put("c", 3);
         map.put("b", 2);
         map.put("a", 1);
-        
+
 
         StringBuilder sb = new StringBuilder();
         map.forEach((String k, Integer v) -> {
             sb.append(String.format("%s%s", k,v));
         });
-        
+
         System.out.println(sb.toString());
     }
-    
+
     public static void main(String[] args) {
         StreamBasicos stream = new StreamBasicos();
         stream.ejecutar();
     }
 
-    
+
 }
