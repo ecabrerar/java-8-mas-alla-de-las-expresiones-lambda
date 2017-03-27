a53e0655ba7ac90fdaa80009248ff10d6516a914
ecabrerar <eudris@gmail.com>
2017-03-27 18:10:44 -0400

    Trabajando con TemporalQuery

--- barcamprd2015/ejemplos/java8-demo/src/test/java/org/ecabrerar/examples/java8/demo/datetime/TrabajarConDateTime.java
+++ barcamprd2015/ejemplos/java8-demo/src/test/java/org/ecabrerar/examples/java8/demo/datetime/TrabajarConDateTime.java
@@ -5,14 +5,18 @@
  */
 package org.ecabrerar.examples.java8.demo.datetime;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 import java.time.ZoneId;
+
 import org.ecabrerar.examples.java8.demo.DiasFestivosRD;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import org.ecabrerar.examples.java8.demo.TemporadaCiclonica;
 import org.junit.Test;
 
 /**
@@ -20,8 +24,8 @@ import org.junit.Test;
  * @author ecabrerar
  */
 public class TrabajarConDateTime {
-    
-    
+
+
     @Test
     public void calcular_diferencia_entre_dos_fecha_java8_usando_period() {
 
@@ -33,14 +37,14 @@ public class TrabajarConDateTime {
 	    int diffInDays = betweenDates.getDays();
 	    int diffInMonths = betweenDates.getMonths();
 	    int diffInYears = betweenDates.getYears();
-        
+
             System.out.println(String.format("%d %d %d", diffInDays,diffInMonths,diffInYears));
 
 	    assertTrue(diffInDays == 12);
 	    assertTrue(diffInMonths >= 0);
 	    assertTrue(diffInYears == 2);
 	}
-    
+
     @Test
     public void es_festivo_el_26_enero_2016() {
 
@@ -50,8 +54,8 @@ public class TrabajarConDateTime {
 
 	    assertTrue(isHoliday);
 	}
-    
-    
+
+
     @Test
     public void obtener_cantidad_dias_que_tienes_un_mes() {
 
@@ -62,26 +66,56 @@ public class TrabajarConDateTime {
 	    assertEquals(30, length);
 
    }
-        
+
    @Test
    public void cantidad_dias_que_tienes_un_mes_usando_lengthOfMonth() {
 
 	    LocalDate date = LocalDate.of(2015, Month.MARCH, 01);
-	   
+
 	    int length2 = date.lengthOfMonth();
 
 	    assertEquals(31, length2);
    }
-   
+
    @Test
    public void obtener_zonahoraria(){
        ZoneId zone = ZoneId.systemDefault();
-       
+
        System.out.println(" "+zone);
        //ZoneId zoneAmerica = ZoneId.of("America/Santo_Domingo");
        ZoneId zoneBerlin = ZoneId.of("Europe/Berlin");
-       
+
        assertNotEquals(zone, zoneBerlin);
 
+   }
+
+   @Test
+   public void es_antes_de_la_temporada_ciclonica () {
+
+       LocalDate date = LocalDate.of(2017, Month.JANUARY, 02);
+
+       Boolean esTemporadaCiclonica = date.query(new TemporadaCiclonica());
+
+       assertFalse(esTemporadaCiclonica);
+   }
+
+   @Test
+   public void es_durante_la_temporada_ciclonica () {
+
+       LocalDate date = LocalDate.of(2017, Month.JUNE, 30);
+
+       Boolean esTemporadaCiclonica = date.query(new TemporadaCiclonica());
+
+       assertTrue(esTemporadaCiclonica);
+   }
+
+   @Test
+   public void es_despues_de_la_temporada_ciclonica () {
+
+       LocalDate date = LocalDate.of(2017, Month.DECEMBER, 30);
+
+       Boolean esTemporadaCiclonica = date.query(new TemporadaCiclonica());
+
+       assertFalse(esTemporadaCiclonica);
    }
 }
